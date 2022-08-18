f127c654ccb5caabacf066e23389ed4aeea5f888
Nestor-Rs <82476269+Nestor-Rs@users.noreply.github.com>
2022-08-17 19:58:23 -0500

    newrelace
    no lea nada

---
+++ secondp/pjtwo/pjtwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pjtwo
+//
+//  Created by Universidad Anahuac on 17/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

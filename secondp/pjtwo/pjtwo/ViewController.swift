8359878dc87dbbc68f8574b1e37eaf7cffb19b89
Nestor-Rs <82476269+Nestor-Rs@users.noreply.github.com>
2022-08-17 21:27:33 -0500

    fakebook
    creacion del diseño del login

--- secondp/pjtwo/pjtwo/ViewController.swift
+++ secondp/pjtwo/pjtwo/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var contrasena: UITextField!
+    @IBOutlet weak var viewText: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        logo.layer.cornerRadius=logo.layer.bounds.width/2
+        logo.clipsToBounds = true
+        viewText.layer.borderWidth=1
+        viewText.layer.borderColor=CGColor.init(red: 200/225, green: 200/225, blue: 200/225, alpha: 1)
+        viewText.layer.cornerRadius=5
     }
 
 
